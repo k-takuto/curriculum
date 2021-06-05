121cf64ee5704554e9d65e0d312137719825feba
k-takuto <81640813+k-takuto@users.noreply.github.com>
2021-06-05 21:03:44 +0900

    2章チェックテスト
    Check2

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,24 @@
+package check;
+
+import constants.Constants;
+
+
+public class Check {
+
+	private static String firstName = "熊井";
+	private static String lastName = "拓斗";
+
+	public static void main(String[] args) {
+		printName(firstName, lastName);
+
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		robotPet.introduce();
+	}
+	
+	private static void printName(String myouji, String namae) {
+		System.out.println(myouji + namae);
+	}
+}
